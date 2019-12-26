e63d48d939c5078518255d68e7706d14586520bd
Andrew <kungpaogao@gmail.com>
2019-12-26 04:04:23 -0500

    Implemented csv creation for line graphs

--- scripts/prototype.py
+++ scripts/prototype.py
@@ -1,12 +1,45 @@
 import pandas as pd
+from string import Template
+
+PRINT_DEBUG = False
+
+
+def print_debug(val):
+    if PRINT_DEBUG:
+        print(val)
+
 
 # ask for file name
-infile = input('Path to input file (including file extension): ')
-outfile = input('Name of output file (excluding file extension): ') + '.csv'
-limit_pre = input('Dataset size (default: 100000): ')
-limit = int(limit_pre) if limit_pre else 100000
+# infile = input('Path to input file (no file extension): ') + '.json'
+infile = "data/msg.json"
 
-dataframe = pd.read_json(infile)
+df = pd.read_json(infile)
 
-print(dataframe)
-print(len(dataframe.index))
+# print(dataframe)
+print_debug(len(df.index))
+print_debug(df.columns)
+# print(list(df["sender_name"]))
+sender_list = list(df["sender_name"])
+sender_set = set(sender_list)
+sender_dict = {}
+
+for s in sender_set:
+    sender_dict[s] = sender_list.count(s)
+
+print_debug(sender_dict)
+
+ts_df = pd.DataFrame(data=df, columns=["timestamp_ms", "sender_name"])
+print_debug(ts_df)
+
+print_debug(">>> filter")
+
+df_list = []
+for s in sender_set:
+    sdf = ts_df[ts_df.sender_name == s]
+    df_list.append(sdf)
+
+out = Template("out/out$ind.csv")
+for i, d in enumerate(df_list):
+    d.to_csv(out.substitute(ind=i), index=False, sep=",")
+
+print("end")
